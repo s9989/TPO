0086bc1fa34b78289cc5f6f54cda2acd6fe74a10
trynis <czterolistny@gmail.com>
2019-03-13 23:17:18 +0100

    getting closer

--- TPO1_KJ_S9989/src/zad1/Futil.java
+++ TPO1_KJ_S9989/src/zad1/Futil.java
@@ -1,50 +1,73 @@
 package zad1;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.file.FileVisitor;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.nio.charset.Charset;
+import java.nio.file.*;
 
 public class Futil 
 {
 	public static void processDir(String dirName, String resultFileName)
 	{
+		System.out.println(Charset.defaultCharset().name());
+
+		try {
+			String fp = "write.txt";
+			FileOutputStream fos = new FileOutputStream(fp);
+			FileChannel fileChannel2 = fos.getChannel();
+			byte[] inputBytes = "".getBytes();
+			ByteBuffer buffer2 = ByteBuffer.wrap(inputBytes);
+			int noOfBytesWritten = fileChannel2.write(buffer2);
+			fileChannel2.close();
+			fos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Żółć");
 		Path path = Paths.get(dirName + "/test.txt");
 		ByteBuffer buffer = ByteBuffer.allocate(10);
 		
 		try {
-			
+
 			Path p = Paths.get(dirName);
 			FileVisitor<Path> fv = new MyFileVisitor();
 			Files.walkFileTree(p, fv);
-			
-			
-			
-			
-			FileChannel fileChannel = FileChannel.open(path, StandardOpenOption.READ);
-			
-			int bytesRead = fileChannel.read(buffer);
-			
-			while (bytesRead != -1) {
-
-			  buffer.flip();  //make buffer ready for read
-
-			  while(buffer.hasRemaining()){
-			      System.out.print((char) buffer.get()); // read 1 byte at a time
-			  }
-
-			  buffer.clear(); //make buffer ready for writing
-			  bytesRead = fileChannel.read(buffer);
-			}
-			
+		} catch (NoSuchFileException e) {
+			System.out.print("Brak katalogu: ");
+			System.out.println(e.getMessage());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
+		try {
+
+			FileChannel fileChannel = FileChannel.open(path, StandardOpenOption.READ);
+			int bytesRead = fileChannel.read(buffer);
+
+			while (bytesRead != -1) {
+
+				buffer.flip();  //make buffer ready for read
+
+				while(buffer.hasRemaining()){
+					System.out.print((char) buffer.get()); // read 1 byte at a time
+				}
+
+				buffer.clear(); //make buffer ready for writing
+				bytesRead = fileChannel.read(buffer);
+			}
+
+		} catch (NoSuchFileException e) {
+			System.out.print("Brak katalogu: ");
+			System.out.println(e.getMessage());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println();
+
 		System.out.println(dirName);
 		System.out.println(resultFileName);
 	}
--- TPO1_KJ_S9989/src/zad1/MyFileVisitor.java
+++ TPO1_KJ_S9989/src/zad1/MyFileVisitor.java
@@ -1,47 +1,83 @@
 package zad1;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.BasicFileAttributes;
 
-public class MyFileVisitor extends SimpleFileVisitor<Path> 
-{
-	@Override
-	public FileVisitResult visitFile(Path arg0, BasicFileAttributes arg1) throws IOException {
-		
-		ByteBuffer buffer = ByteBuffer.allocate(10);
-		
+public class MyFileVisitor extends SimpleFileVisitor<Path> {
+    @Override
+    public FileVisitResult visitFile(Path arg0, BasicFileAttributes arg1) throws IOException {
+
+        ByteBuffer buffer = ByteBuffer.allocate(30);
+
 //		System.out.println(arg0);
-		
-		FileChannel fileChannel = FileChannel.open(arg0, StandardOpenOption.READ);
-		
-		int bytesRead = fileChannel.read(buffer);
-		
-		String s = new String();
-		
-		while (bytesRead != -1) {
 
-		  buffer.flip();  //make buffer ready for read
+        FileChannel fileChannel = FileChannel.open(arg0, StandardOpenOption.READ);
 
-		  while(buffer.hasRemaining()){
-			  s = s + (char) buffer.get();
-		      //System.out.print((char) buffer.get()); // read 1 byte at a time
-		  }
+        int bytesRead = fileChannel.read(buffer);
 
-		  buffer.clear(); //make buffer ready for writing
-		  bytesRead = fileChannel.read(buffer);
-		}
-		
-		System.out.println(s);
-		String decoded = new String(s.getBytes("CP1250"), "UTF-8");
-		System.out.println(decoded);
-		System.out.println();
-		
-		return super.visitFile(arg0, arg1);
-	}
+        String s = "";
+
+        StringBuilder sb = new StringBuilder();
+
+        while (bytesRead != -1) {
+
+
+
+            buffer.flip();  //make buffer ready for read
+
+            Charset cs = Charset.forName("windows-1250");
+            System.out.print("OK: ");
+//            System.out.print(cs.decode(buffer));
+            System.out.println("|");
+
+            sb.append(cs.decode(buffer));
+
+            while (buffer.hasRemaining()) {
+                s = s + (char) buffer.get();
+                //System.out.print((char) buffer.get()); // read 1 byte at a time
+            }
+
+
+            buffer.clear(); //make buffer ready for writing
+            bytesRead = fileChannel.read(buffer);
+        }
+
+        fileChannel.close();
+
+        System.out.print("END: ");
+        System.out.print(sb.toString());
+        System.out.println("|");
+        System.out.println(s);
+        String decoded = new String(s.getBytes("windows-1250"), StandardCharsets.UTF_8);
+
+
+        System.out.println(decoded);
+        System.out.println();
+        System.out.println();
+
+        String fp = "write.txt";
+        FileOutputStream fos = new FileOutputStream(fp, true);
+        FileChannel fileChannel2 = fos.getChannel();
+
+        fileChannel2.position(fileChannel2.size());
+
+        byte [] inputBytes = sb.toString().getBytes();
+        ByteBuffer buffer2 = ByteBuffer.wrap(inputBytes);
+        int noOfBytesWritten = fileChannel2.write(buffer2);
+        fileChannel2.close();
+        fos.close();
+
+
+        return super.visitFile(arg0, arg1);
+    }
 }
