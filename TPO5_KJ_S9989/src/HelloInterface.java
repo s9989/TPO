03c90ac2706bb2513b3db04bbde43e64dd984aff
Jakub Kuryłowicz SOFTFIN SP. Z O.O <jakub.kurylowicz@gnb.pl>
2019-05-17 13:35:09 +0200

    done

--- TPO5_KJ_S9989/src/HelloInterface.java
+++
@@ -1,7 +0,0 @@
-//HelloInterface.java
-import java.rmi.Remote;
-
-public interface HelloInterface extends java.rmi.Remote
-{
-    void sayHello( String from ) throws java.rmi.RemoteException;
-}
--- TPO5_KJ_S9989/src/zad1/Client.java
+++ TPO5_KJ_S9989/src/zad1/Client.java
@@ -3,9 +3,16 @@ package zad1;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.rmi.PortableRemoteObject;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.rmi.RemoteException;
 
 public class Client
 {
+    static private GraphicsConfiguration gc;
+
     public static void  main( String args[] )
     {
         Context ic;
@@ -23,6 +30,9 @@ public class Client
             phoneDirectory = (PhoneDirectoryInterface) PortableRemoteObject.narrow(
                     objref, PhoneDirectoryInterface.class);
 
+            renderGui(phoneDirectory);
+
+            /*
             String number;
             phoneDirectory.addPhoneNumber("jake", "789");
 
@@ -33,11 +43,98 @@ public class Client
 
             number = phoneDirectory.getPhoneNumber("anna");
             System.out.println(number);
+            */
 
         } catch( Exception e ) {
             System.err.println( "Exception " + e + "Caught" );
             e.printStackTrace( );
             return;
         }
+    }
+
+    private static void renderGui(PhoneDirectoryInterface phoneDirectory)
+    {
+        JFrame frame = new JFrame(gc);
+        frame.setTitle("Client");
+        frame.setSize(400, 300);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+
+        JPanel listPane = new JPanel();
+        listPane.setLayout(new BoxLayout(listPane, BoxLayout.PAGE_AXIS));
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+
+        JPanel top = new JPanel();
+
+        top.add(new JLabel("PhoneBook"));
+
+        JButton read = new JButton("read");
+        read.setBackground(Color.GREEN);
+        top.add(read);
+
+        JButton save = new JButton("save");
+        save.setBackground(Color.RED);
+        top.add(save);
+
+        panel.add(top);
+
+        JTextField ta_read = new JTextField();
+        panel.add(ta_read);
+
+        JTextField ta_write = new JTextField();
+        panel.add(ta_write);
+
+        top.putClientProperty("input", ta_read);
+        top.putClientProperty("output", ta_write);
+
+        listPane.add(panel);
+
+        read.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JButton current = (JButton)e.getSource();
+                JPanel parent = (JPanel) current.getParent();
+                JTextField inputField = (JTextField) parent.getClientProperty("input");
+                JTextField outputField = (JTextField) parent.getClientProperty("output");
+                try {
+                    String value = phoneDirectory.getPhoneNumber(inputField.getText());
+                    outputField.setText(value);
+                } catch (RemoteException ex) {
+                    JOptionPane.showMessageDialog(null, "Connection error");
+                }
+            }
+        });
+
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JButton current = (JButton)e.getSource();
+                JPanel parent = (JPanel) current.getParent();
+                JTextField inputField = (JTextField) parent.getClientProperty("input");
+                JTextField outputField = (JTextField) parent.getClientProperty("output");
+                try {
+                    String value = phoneDirectory.getPhoneNumber(inputField.getText());
+                    if (null == value) {
+                        phoneDirectory.addPhoneNumber(inputField.getText(), outputField.getText());
+                        JOptionPane.showMessageDialog(null, "Added");
+                    } else {
+                        phoneDirectory.replacePhoneNumber(inputField.getText(), outputField.getText());
+                        JOptionPane.showMessageDialog(null, "Replaced");
+                    }
+
+                } catch (RemoteException ex) {
+                    JOptionPane.showMessageDialog(null, "Connection error");
+                }
+            }
+        });
+
+        frame.add(listPane);
+
+        frame.pack();
+        frame.setVisible(true);
     }
 }
